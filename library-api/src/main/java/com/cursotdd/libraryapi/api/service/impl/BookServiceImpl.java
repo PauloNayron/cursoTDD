036965905fd610ec8f67b91b66c1c64b47dac1d9
PauloNayron <paulonayron.s@hotmail.com>
2020-04-06 22:15:56 -0300

    feat: Teste Update de Book

--- library-api/src/main/java/com/cursotdd/libraryapi/api/service/impl/BookServiceImpl.java
+++ library-api/src/main/java/com/cursotdd/libraryapi/api/service/impl/BookServiceImpl.java
@@ -33,4 +33,9 @@ public class BookServiceImpl implements com.cursotdd.libraryapi.api.service.Book
     public void delete(Book book) {
         
     }
+
+    @Override
+    public Book update(Book book) {
+        return null;
+    }
 }
--- library-api/src/test/java/com/cursotdd/libraryapi/api/resource/BookControllerTest.java
+++ library-api/src/test/java/com/cursotdd/libraryapi/api/resource/BookControllerTest.java
@@ -172,6 +172,50 @@ public class BookControllerTest {
                 .andExpect(status().isNotFound());
     }
 
+    @Test
+    @DisplayName("Deve atualizar um livro.")
+    public void updateBookTest () throws Exception {
+        // cenario
+        Long id = 1L;
+        String json = new ObjectMapper().writeValueAsString(createNewBook());
+        Book updatingBook = Book.builder().id(1L).title("some title").author("some author").isbn("321").build();
+        BDDMockito.given(service.getById(id)).willReturn(Optional.of(updatingBook));
+
+        Book updatedBook = Book.builder().id(1L).author("Artur").title("As aventuras").isbn("321").build();
+        BDDMockito.given(service.update(updatingBook)).willReturn(updatedBook);
+        // execucao
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .put(BOOK_API.concat("/" + 1))
+                .content(json)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+        // verificacao
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("id").value(id))
+                .andExpect(jsonPath("title").value(createNewBook().getTitle()))
+                .andExpect(jsonPath("author").value(createNewBook().getAuthor()))
+                .andExpect(jsonPath("isbn").value("321"));
+    }
+
+    @Test
+    @DisplayName("Deve retornar 404 ao tentar atualizar um livro inexistente.")
+    public void updateInexistentBookTest () throws Exception {
+        // cenario
+        String json = new ObjectMapper().writeValueAsString(createNewBook());
+        BDDMockito.given(service.getById(Mockito.anyLong()))
+                .willReturn(Optional.empty());
+        // execucao
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .put(BOOK_API.concat("/" + 1))
+                .content(json)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+        // verificacao
+        mvc.perform(request)
+                .andExpect(status().isNotFound());
+    }
+
     private BookDTO createNewBook() {
         return BookDTO.builder().author("Artur").title("As aventuras").isbn("001").build();
     }
