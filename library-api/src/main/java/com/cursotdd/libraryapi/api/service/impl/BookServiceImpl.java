044986cad5207aca48c046c27b869c14465ea65d
PauloNayron <paulonayron.s@hotmail.com>
2020-04-07 00:27:38 -0300

    Criação de Service de Busca Paginada de Livros

--- library-api/src/main/java/com/cursotdd/libraryapi/api/service/impl/BookServiceImpl.java
+++ library-api/src/main/java/com/cursotdd/libraryapi/api/service/impl/BookServiceImpl.java
@@ -3,6 +3,8 @@ package com.cursotdd.libraryapi.api.service.impl;
 import com.cursotdd.libraryapi.api.model.entity.Book;
 import com.cursotdd.libraryapi.exception.BusinessException;
 import com.cursotdd.libraryapi.model.repository.BookRepository;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -49,6 +51,12 @@ public class BookServiceImpl implements com.cursotdd.libraryapi.api.service.Book
 
     @Override
     public Page<Book> find(Book filter, Pageable pageRequest) {
-        return null;
+        Example<Book> example = Example.of(filter,
+                ExampleMatcher
+                        .matching()
+                        .withIgnoreCase()
+                        .withIgnoreNullValues()
+                        .withStringMatcher( ExampleMatcher.StringMatcher.CONTAINING ));
+        return repository.findAll(example, pageRequest);
     }
 }
